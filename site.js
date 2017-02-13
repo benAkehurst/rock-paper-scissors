98929d6e0965814be4d750a40faa24109492fbc3
mistabenjo <benakehurst@hotmail.com>
2017-02-13 17:47:53 +0200

    v1
    HTML & CSS complete
    Begining JS

---
+++ site.js
@@ -0,0 +1,22 @@
+(function(){
+
+	"use strict";
+
+	var userChoice;
+
+	$(function(){
+
+	}); // End of jQuery Self Invoked
+
+	function computerChoice(){
+		
+		var computerChoice = " ";
+		var x = Math.random();
+
+		if(x < 0.37){
+			computerChoice = "Rock";
+		}
+		if else ()
+	}
+
+})(); // End of Javascript
